c01ff5d9da6df454b56977189eab736a0325df36
hiddegrei <grei0005@hz.nl>
2022-12-15 10:40:44 +0100

    strava

--- src/components/strava/StravaMain.js
+++ src/components/strava/StravaMain.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import "../../css/StravaMain.css";
 
 function StravaMain(props) {
+    useEffect(()=>{
+         let path = window.location.search;
+
+         let code1 = path.split("&");
+         let code2 = code1[1].split("=");
+         let code = code2[1];
+         let scopes = code1[2];
+    },[])
     return (
         <div className="strava">
             <div className="strava__header">
